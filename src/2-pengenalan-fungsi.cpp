0354590a98d555378ef803c74a1137802e972a10
emptywork <22065214+EmptyWork@users.noreply.github.com>
2021-12-07 08:58:06 +0900

    Tambah: pengenalan fungsi

---
+++ src/2-pengenalan-fungsi.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int tambah(int a, int b) {
+  return a + b;
+}
+
+int main() {
+  std::cout << "Contoh pembuatan Fungsi:" << std::endl;
+  std::cout << "4 + 6 = " << tambah(4, 6);
+  return 0;
+}
---
+++ src/comment/2b-pengenalan-fungsi.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+/* 
+  Menginisialisasi sebuah fungsi dengan nama tambah
+  yang memiliki 2 parameter
+  @param a - Int
+  @param b - Int
+  dan akan mengembalikan
+  @return a + b - Int
+*/
+int tambah(int a, int b) {
+  return a + b;
+}
+
+int main() {
+  std::cout << "Contoh pembuatan Fungsi:" << std::endl;
+  std::cout << "4 + 6 = " << tambah(4, 6);
+  return 0;
+}
