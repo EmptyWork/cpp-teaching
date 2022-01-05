d32f5bcc146d37b0adfad80e935b92a52d6ea467
emptywork <22065214+EmptyWork@users.noreply.github.com>
2022-01-05 20:06:43 +0900

    Ubah: src/data-types/native | `f` untuk float

--- src/data-types/native/native_comment.cpp
+++ src/data-types/native/native_comment.cpp
@@ -1,25 +1,29 @@
-int main() {
-  /*
-    Berikut ini merupakan tipe-tipe data bawaan (native)
-    pada C++20 atau standard terbaru C++(2021)
+/**
+ * @brief Tipe-tipe data dasar yang merupakaan bawaan
+ * dari C dan C++ 
+ * 
+ * @details Berikut ini merupakan tipe-tipe data bawaan (native) 
+ * pada C++20 atau standard terbaru C++(2021)
+ * Adapun penamaan variable mengikuti standard yang
+ * digunakan secara umum
+ * 
+ * @link https://chaste.cs.ox.ac.uk/trac/raw-attachment/wiki/CodingStandardsStrategy/codingStandards.pdf @endlink
+ */
 
-    * Adapun penamaan variable mengikuti standard yang
-    * digunakan secara umum
-    * source : https://chaste.cs.ox.ac.uk/trac/raw-attachment/wiki/CodingStandardsStrategy/codingStandards.pdf
-  
-  */
+int main() {
   int angka_satu = 1;
   char karakter_x = 'x';
   bool benar = true;
   double angka_3_titik_4 = 3.4;
   float angka_2_titik_1 = 2.1f;
-  /*
-    wchar_t atau wide character string merupakan bentuk data
-    yang diutamakan untuk karakter-karakter (unicode) yang melebihi
-    bentuk dari besaran char (ISO/IEC 10646:2003)
-  */
+  
+  /**
+   * @brief wchar_t atau wide character
+   * @details wchar_t atau wide character string merupakan bentuk data
+   * yang diutamakan untuk karakter-karakter (unicode) yang melebihi 
+   * bentuk dari besaran char (ISO/IEC 10646:2003)
+   */
   wchar_t sebuah_karakter_unicode;  
-  // void kosong;
   
   return 0;
 }
--- src/data-types/native/src/native.cpp
+++ src/data-types/native/src/native.cpp
@@ -3,7 +3,7 @@ int main() {
   char karakter_x = 'x';
   bool benar = true;
   double angka_3_titik_4 = 3.4;
-  float angka_2_titik_1 = 2.1;
+  float angka_2_titik_1 = 2.1f;
   wchar_t sebuah_karakter_unicode;  
   // void kosong;
   
