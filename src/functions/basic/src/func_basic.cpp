72bd6630053dddd06c2b6d13485c2e4b7a5c3de0
emptywork <22065214+EmptyWork@users.noreply.github.com>
2022-01-05 21:21:37 +0900

    Ubah: src/functions/basic - `variant`, penjelasan

--- src/functions/basic/src/func_basic.cpp
+++ src/functions/basic/src/func_basic.cpp
@@ -1,11 +1,31 @@
 #include <iostream>
+#define NL '\n'
 
 int tambah(int a, int b) {
   return a + b;
 }
 
+bool kurangdari2(int a) {
+  return (a < 2) ? true : false;
+}
+
+float keliling(float a, float b) {
+  return 2 * (a + b);
+}
+
+double luas(double a) {
+  return a * a;
+}
+
+const char* benarsalah(int a) {
+  return (a == 1) ? "Benar" : "Salah";
+}
+
 int main() {
-  std::cout << "Contoh pembuatan Fungsi:" << std::endl;
-  std::cout << "4 + 6 = " << tambah(4, 6);
+  std::cout << "Contoh pembuatan Fungsi: " << NL;
+  std::cout << "Hasil dari 4 + 6: " << tambah(4, 6) << NL;
+  std::cout << "Apakah -1 kurang dari 2: " << benarsalah(kurangdari2(-1)) << NL; 
+  std::cout << "Keliling dari Persegi Panjang (5, 8): " << keliling(4.5f, 2.4f) << NL; 
+  std::cout << "Luas dari Kotak (4): " << luas(4.5) << NL; 
   return 0;
 }
