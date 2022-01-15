87c4d6b7ecfa86e4199157de230fe1cc32b404cc
emptywork <22065214+EmptyWork@users.noreply.github.com>
2022-01-15 13:42:54 +0900

    Ubah: `clean.sh` - `\n`

--- clean.sh
+++ clean.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 
 echo "CPP Teaching Copyright (C) 2021  EmptyWork"
-echo "Purging all \`build\` and \`obj\` folders"
-echo " "
+echo "Purging all \`build\` and \`obj\` folders\n"
 find . -type d -name "build" -print0 | xargs -0  rm -rfv
 find . -type d -name "obj" -print0 | xargs -0  rm -rfv
