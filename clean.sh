bc20c0ca84b7eb3babb546d60c0da427bcfea3e4
emptywork <22065214+EmptyWork@users.noreply.github.com>
2022-01-16 13:33:49 +0900

    Ubah: `clean.sh`

--- clean.sh
+++ clean.sh
@@ -1,5 +1,12 @@
 #!/bin/sh
 
+# Copyright (C) 2021 EmptyWork
+# 
+# clean.sh is a bash script 
+# aim to help user to remove any unnecessary
+# file such as `obj` and `exe`
+#
+
 echo "CPP Teaching Copyright (C) 2021  EmptyWork"
 echo "Purging all \`build\` and \`obj\` folders\n"
 find . -type d -name "build" -print0 | xargs -0  rm -rfv
