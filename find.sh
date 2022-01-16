19fbc0c2ccad2101e5401a622eb029872540ad01
emptywork <22065214+EmptyWork@users.noreply.github.com>
2022-01-16 13:33:25 +0900

    Ubah: `find.sh` - komen

--- find.sh
+++ find.sh
@@ -1,5 +1,15 @@
 #!/bin/sh
 
+# Copyright (C) 2021 EmptyWork
+# 
+# find.sh is a bash script 
+# aim to help user to looks for certain file or application
+# based on name that provide
+# 
+# default filename : main
+# usage find.sh filename
+#
+
 if [ $1 ] 
   then 
     text=$1
