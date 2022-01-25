631c7edfb1f85b93c227772aa735dc964f9bdd7c
emptywork <22065214+EmptyWork@users.noreply.github.com>
2022-01-25 17:12:04 +0900

    Ubah: `namespace_basic.cpp` - namespace bersarang

--- src/namespace/basic/src/namespace_basic.cpp
+++ src/namespace/basic/src/namespace_basic.cpp
@@ -5,6 +5,11 @@ namespace foo
   int five = 5;
   float sixpointseven = 6.7f;
 
+  namespace bar {
+    int five = 5;
+    float sixpointseven = 6.7f;
+  }
+
   void show() {
     std::cout << "This is a function";
   }
@@ -13,8 +18,12 @@ namespace foo
 int main() {
 
   std::cout << foo::five << std::endl;
+
+  std::cout << foo::bar::five << std::endl;
   
   std::cout << foo::sixpointseven << std::endl;
+  
+  std::cout << foo::bar::sixpointseven << std::endl;
   
   foo::show();
 
