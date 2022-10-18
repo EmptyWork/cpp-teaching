38c833437a96fc9e52ab040bedb3bba6c91fb961
emptywork <22065214+EmptyWork@users.noreply.github.com>
2022-10-18 18:44:58 +0900

    Ubah `src/namespace/class` tambahkan beberapa contoh

--- src/data-types/basic/src/basic.cpp
+++ src/data-types/basic/src/basic.cpp
@@ -1,3 +1,5 @@
+#include <iostream>
+
 int main() {
   int angka_satu = 1;
   char karakter_x = 'x';
@@ -6,6 +8,18 @@ int main() {
   float angka_2_titik_1 = 2.1f;
   wchar_t sebuah_karakter_unicode;  
   // void kosong;
+
+  std::cout << angka_satu;
+
+  std::cout << karakter_x;
+  
+  std::cout << benar;
+  
+  std::cout << angka_3_titik_4;
+  
+  std::cout << angka_2_titik_1;
+  
+  std::cout << sebuah_karakter_unicode;
   
   return 0;
 }
